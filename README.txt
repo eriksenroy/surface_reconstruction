958dbad3ffe749481947c0938618085102245971
eriksenroy <eriksen.roy@gmail.com>
2020-07-10 07:46:23 +0200

    adding readme

---
+++ README.txt
@@ -0,0 +1,3 @@
+Surface Reconstruction.
+
+Inspired by DPPTAM.
--- include/surface_reconstruction/vo_system.h
+++ include/surface_reconstruction/vo_system.h
@@ -4,5 +4,49 @@
 
 #ifndef SURFACE_RECONSTRUCTION_VO_SYSTEM_H
 #define SURFACE_RECONSTRUCTION_VO_SYSTEM_H
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <vector>
+#include <cstdio>
 
+
+using namespace std;
+
+// TIC - TOC
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <math.h>
+
+
+#include <boost/thread/thread.hpp>
+#include <iostream>
+#include <stdio.h>
+#include <boost/filesystem.hpp>
+
+
+#include <ros/ros.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/Imu.h>
+
+#include <visualization_msgs/Marker.h>
+
+/////ROS IMAGE SUBSCRIBER
+#include <iostream>
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <image_transport/image_transport.h>
+
+#include <surface_reconstruction/tracking.h>
+#include <surface_reconstruction/mapping.h>
 #endif //SURFACE_RECONSTRUCTION_VO_SYSTEM_H
+
+
+class vo_system
