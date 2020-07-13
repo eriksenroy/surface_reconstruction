b2aa7b32673c8f1bc250896dde76fc899f5b9a73
eriksenroy <eriksen.roy@gmail.com>
2020-07-13 11:52:06 +0200

    add

---
+++ cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "surface_reconstruction"
+PROJECT_SPACE_DIR = "/home/roy/ros_workspace/src/surface_reconstruction/cmake-build-debug/devel"
+PROJECT_VERSION = "0.0.0"
